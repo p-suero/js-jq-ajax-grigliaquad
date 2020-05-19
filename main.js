44c0b0cef1019e6e4c7e4dad767525adecb4d3b0
Paolo <paolo.suero@gmail.com>
2020-05-19 16:48:08 +0200

    Esercizio completato (con bonus handlebars)

--- main.js
+++ main.js
@@ -1,38 +1,41 @@
 $(document).ready(function() {
     //recupero la struttura html del template
     var template_html = $("#entry-template").html();
-    // preparo la funzione al fine di utilizzare il template con handlebars
+    //preparo la funzione al fine di utilizzare il template con handlebars
     var template_function = Handlebars.compile(template_html);
-    //creo l'oggetto con i valori del placeholder dell Handlebars
-    var quadrato = {
-        "forma" : "quadrato",
-        "tipo"  : "numero"
-    }
-    //tramite handlebars preparo l'html con le proprietà dell'oggetto
-    var html_finale = template_function(quadrato);
-    //tramite il ciclo for aggiungo per 36 volte i quadrati nella pagina htmk
+    //tramite handlebars preparo l'html
+    var html_finale = template_function();
+    //tramite il ciclo for aggiungo per 36 volte i quadrati nella pagina html
     for (var i = 0; i < 36; i++) {
         $("#griglia").append(html_finale)
     }
 
     //intercetto il click sul quadrato tramite la funzione on, vista l'aggiunta di elementi dinamici
     $("#griglia").on("click", ".quadrato", function() {
+        //salvo la variabile con l'elemento corrente al fine di riutilizzarla nella funzione ajax
+        var elemento_corrente = $(this);
         //faccio una chiamata ajax per reperire un numero random da 1 a 9
         $.ajax({
-            "url" : "https://flynn.boolean.careers/exercises/api/random/int",
-            "method" : "GET",
-            "success" : function(data) {
+            "url": "https://flynn.boolean.careers/exercises/api/random/int",
+            "method": "GET",
+            "success": function(data) {
+                //recupero il numero restituito dall'api
                 var numero_random_pc = data.response
-                console.log(numero_random_pc);
-                console.log($(this));
-                //aggiungo il numero nel quadrato corrente
-                var add_numero = $(this).find(".numero").text(numero_random_pc)
-                //creo la condizione
+                //verifico se nel quadrato è stato già inserito un numero
+                if (elemento_corrente.find(".numero").text() == "") {
+                    //aggiungo il numero nel quadrato corrente
+                    elemento_corrente.find(".numero").text(numero_random_pc);
+                    //creo la condizione per impostare il background al quadrato corrente
+                    if (numero_random_pc <= 5) {
+                        elemento_corrente.addClass("yellow");
+                    } else {
+                        elemento_corrente.addClass("green");
+                    }
+                }
             },
-            "error" : function() {
+            "error": function() {
                 alert("si è verificato un errore, impossibile recuperare il dato richiesto")
             }
         })
-    } )
-
+    })
 })
